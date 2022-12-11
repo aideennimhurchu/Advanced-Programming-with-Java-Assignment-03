d84ad59fbc48e484b2e4a24b0bba9ceb4f45e1ee
aideenmurphy <120323726@umail.ucc.ie>
2022-12-11 13:45:16 +0000

    Wrote basic unit test for long parameter Colour constructor.

--- src/main/java/ColourTester.java
+++ src/main/java/ColourTester.java
@@ -26,4 +26,18 @@ public class ColourTester {
         boolean actual2 = c1.equals(c2);
         assertEquals(c1, c2);
         }
+
+        public void testLongRGBConstructor(){
+            //create colour objects to use for testing
+            Colour c4 = new Colour("173827395829384571628723");
+            Colour c5 = new Colour("019285627491829482736162");
+            Colour c6 = new Colour("173827395829384571628723");
+
+            //test that constructor is functioning properly
+            assertEquals("17382739", c4.getRed2());
+            assertEquals("74918294", c5.getBlue2());
+            assertEquals("71628723", c5.getGreen2());
+        }
+
+
 }
